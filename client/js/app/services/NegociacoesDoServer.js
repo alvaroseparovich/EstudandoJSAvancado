97da50806a4b751d1d426c5577a4b979e223feec
Alvaro Separovich <alvasepro@gmail.com>
2017-05-24 19:24:00 -0300

    curso JS Avançado II: Aula 5 completa

--- client/js/app/services/NegociacoesDoServer.js
+++ client/js/app/services/NegociacoesDoServer.js
@@ -1,32 +1,25 @@
 class NegociacoesDoServer {
 	
+	constructor(){
+
+		this._http = new HttpService;
+
+	}
+
 	negociacoesDaSemana(){
 
-		return new Promise((resposta, erro)=>{
-			let xhr = new XMLHttpRequest();
-			xhr.open('GET', 'negociacoes/semana');
-
-			xhr.onreadystatechange = () => {
-				if(xhr.readyState == 4){
-					if(xhr.status == 200){
-						resposta(JSON
-							.parse(xhr.responseText)
-							.map(obj=> new Negociacao(new Date(obj.data),obj.quantidade, obj.valor))
-						);
-					}else{
-						console.log(xhr.responseText);
-						erro("Não foi possível receber as informações do servidor.");
-					}
-
-				}
-
-			}
-
-			xhr.send();
+		return new Promise((resposta, error)=>{
 			
-
-			}
-		)
+			this._http
+			.get('negociacoes/semana')
+			.then(negociacoes => {
+				resposta(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))
+			})
+			.catch(erro => {
+				console.log(erro);
+				error('Não foi possível continuar a importação')
+			})	
+		})
 
 	}
 
